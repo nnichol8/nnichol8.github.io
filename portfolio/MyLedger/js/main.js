de161e285ce99051fddfea80fea078546ea5291a
Nicole Williams <nnichol8@gmail.com>
2020-02-08 17:22:43 -0500

    add form functionality

--- portfolio/MyLedger/js/main.js
+++ portfolio/MyLedger/js/main.js
@@ -44,7 +44,7 @@ window.addEventListener('scroll', function() {
 //////////////////////////////////////////////////////////////////////////////
 
       //Smooth Scrolling
-      $('#navbar a, .btn').on('click', function(event) {
+      $('#navbar a').on('click', function(event) {
           if (this.hash !== '') {
             event.preventDefault();
 
